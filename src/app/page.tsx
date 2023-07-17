20882b51b4270243d3395128649dc6869e06456f
Kaique <kaique.santos@duosystem.com.br>
2023-07-17 18:39:13 -0300

    Creation footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 
@@ -5,88 +6,90 @@ import './home.css';
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-      <Hero />
-
-      <h2 className="title-class">Color</h2>
-      <div className="flex justify-evenly w-11/12 pl-24">
-        <div>
-          <div className="green-div"></div>
-          <span className="text-normal font-semibold pl-9">#40DDB6</span>
+      <main>
+        <Hero />
+        <h2 className="title-class">Color</h2>
+        <div className="flex justify-evenly w-11/12 pl-24">
+          <div>
+            <div className="green-div"></div>
+            <span className="text-normal font-semibold pl-9">#40DDB6</span>
+          </div>
+          <div>
+            <div className="purple-div"></div>
+            <span className="text-normal font-semibold pl-9">#6B78E5</span>
+          </div>
+          <div>
+            <div className="black-div"></div>
+            <span className="text-normal font-semibold pl-9">#37393F</span>
+          </div>
+          <div>
+            <div className="grey-div"></div>
+            <span className="text-normal font-semibold pl-9">#7D7D7D</span>
+          </div>
+          <div>
+            <div className="light-div"></div>
+            <span className="text-normal font-semibold pl-9">#F4F4F4</span>
+          </div>
         </div>
-        <div>
-          <div className="purple-div"></div>
-          <span className="text-normal font-semibold pl-9">#6B78E5</span>
+
+        <h2 className="title-class">Typography</h2>
+        <div className="typography-class">
+          <h1>Heading H1 Title</h1>
+          <h2>Heading H2 Title</h2>
+          <h3>Heading H3 Title</h3>
+          <h4>Heading H4 Title</h4>
+          <h5>Heading H5 Title</h5>
+          <h6>Heading H6 Title</h6>
         </div>
-        <div>
-          <div className="black-div"></div>
-          <span className="text-normal font-semibold pl-9">#37393F</span>
+
+        <h2 className="title-class">Paragraph</h2>
+        <div className="paragraph-class">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+            containing Lorem Ipsum passages, and more recently with.
+          </p>
         </div>
-        <div>
-          <div className="grey-div"></div>
-          <span className="text-normal font-semibold pl-9">#7D7D7D</span>
+
+        <h2 className="title-class">Lists</h2>
+        <div className="list-class">
+          <ul>
+            <li>The printing and typesetting industry.</li>
+            <li>Lorem Ipsum has been the industry's standard</li>
+            <li>It has survived not only five centuries.</li>
+          </ul>
+          <ol>
+            <li> The printing and typesetting industry.</li>
+            <li> Lorem Ipsum has been the industry's standard</li>
+            <li> It has survived not only five centuries.</li>
+          </ol>
         </div>
-        <div>
-          <div className="light-div"></div>
-          <span className="text-normal font-semibold pl-9">#F4F4F4</span>
+
+        <h2 className="title-class">Blockquotes</h2>
+        <div className="blockquotes-class">
+          <p>
+            The job market of the future will consist of those jobs that robots cannot perform. 
+            Our blue-collar work is pattern recognition, making sense of what you see. 
+            Gardeners will still have jobs because every garden is different. 
+            The same goes for construction workers. The losers are white-collar workers, low-level accountants, 
+            brokers, and agents.
+          </p>
         </div>
-      </div>
 
-      <h2 className="title-class">Typography</h2>
-      <div className="typography-class">
-        <h1>Heading H1 Title</h1>
-        <h2>Heading H2 Title</h2>
-        <h3>Heading H3 Title</h3>
-        <h4>Heading H4 Title</h4>
-        <h5>Heading H5 Title</h5>
-        <h6>Heading H6 Title</h6>
-      </div>
-
-      <h2 className="title-class">Paragraph</h2>
-      <div className="paragraph-class">
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, 
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-          containing Lorem Ipsum passages, and more recently with.
-        </p>
-      </div>
-
-      <h2 className="title-class">Lists</h2>
-      <div className="list-class">
-        <ul>
-          <li>The printing and typesetting industry.</li>
-          <li>Lorem Ipsum has been the industry's standard</li>
-          <li>It has survived not only five centuries.</li>
-        </ul>
-        <ol>
-          <li> The printing and typesetting industry.</li>
-          <li> Lorem Ipsum has been the industry's standard</li>
-          <li> It has survived not only five centuries.</li>
-        </ol>
-      </div>
-
-      <h2 className="title-class">Blockquotes</h2>
-      <div className="blockquotes-class">
-        <p>
-          The job market of the future will consist of those jobs that robots cannot perform. 
-          Our blue-collar work is pattern recognition, making sense of what you see. 
-          Gardeners will still have jobs because every garden is different. 
-          The same goes for construction workers. The losers are white-collar workers, low-level accountants, 
-          brokers, and agents.
-        </p>
-      </div>
-
-      <h2 className="title-class">Button</h2>
-      <div className="button-class">
-        <div>Button Default</div>
-        <div>Button Default</div>
-        <div>Button Default</div>
-        <div>Button Default</div>
-      </div>
-    </main>
+        <h2 className="title-class">Button</h2>
+        <div className="button-class">
+          <div>Button Default</div>
+          <div>Button Default</div>
+          <div>Button Default</div>
+          <div>Button Default</div>
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,164 @@
+import Phone from '../../assets/png/footer/phone.png'
+import Mail from '../../assets/png/footer/mail.png'
+import Location from '../../assets/png/footer/localization.png'
+import Logo from '../../assets/png/footer/logo.png'
+
+import Instagram from '../../assets/svg/instagram.svg'
+import Facebook from '../../assets/svg/facebook.svg'
+import Twitter from '../../assets/svg/twitter.svg'
+import Pinterest from '../../assets/svg/pinterest.svg'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import './style.css'
+
+export const Footer = () => {
+    return (
+        <footer className="bg-slate-900">
+            <div className="py-20 px-24">
+                <div className="flex justify-between border-b border-slate-300 w-full pb-6">
+                    <div>
+                        <Image src={Logo} alt="Logo"/>
+                    </div>
+                    <ul className="flex text-white">
+                        <li>
+                            <div className='flex mr-12'>
+                                <div className="flex border-radius-50 bg-slate-800 px-4 py-3">
+                                    <Image src={Phone} alt="Phone" width="20"/>
+                                </div>
+                                <div className="flex items-start flex-col pt-1 ml-2">
+                                    <h4 className="text-base">Call Us</h4>
+                                    <p className='text-xs font-light text-slate-300'>+01 569 896 654</p>
+                                </div>
+                            </div>                     
+                        </li>
+                        <li>
+                            <div className='flex mr-12'>
+                                <div className="flex border-radius-50 bg-slate-800 px-4 py-4 ml-2">
+                                    <Image src={Mail} alt="Mail" width="20"/>
+                                </div>
+                                <div className="flex items-start flex-col pt-1 ml-2">
+                                    <h4 className="text-base">Mail Us</h4>
+                                    <p className='text-xs font-light text-slate-300'>info@example.com</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='flex'>
+                                <div className="flex border-radius-50 bg-slate-800 px-4 py-3 ml-2">
+                                    <Image src={Location} alt="Map" width="14"/>
+                                </div>
+                                <div className="flex items-start flex-col pt-1 ml-2">
+                                    <h4 className="text-base">Location</h4>
+                                    <p className='text-xs font-light text-slate-300'>Amsterdam, 109-74</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-6 flex border-b pb-6">
+                    <ul className="w-full">
+                        <div className='flex'>
+                            <div>
+                                <li>
+                                    <span>🟪</span><span className="title-link">Quick Link</span>
+                                </li>
+                                <li className="mt-3 text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Home</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">About Us</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Blog</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Services</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Contact</Link>
+                                </li>
+                            </div>
+                            <div>
+                                <li>
+                                    <span>🟪</span><span className="title-link">Our Services</span>
+                                </li>
+                                <li className="mt-3 text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Financial Consulting</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Bussiness Management</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Consult & Programs</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Company Management</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Insurance & Finance</Link>
+                                </li>
+                            </div>
+                            <div>
+                                <li>
+                                    <span>🟪</span><span className="title-link">Utility Page</span>
+                                </li>
+                                <li className="mt-3 text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Style Guide</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Licenses</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Instructions</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">Changelog</Link>
+                                </li>
+                                <li className="text-white font-light li-hover leading-relaxed">
+                                    <Link href="/">404 Page</Link>
+                                </li>
+                            </div>
+
+                        </div>
+                    </ul>
+                    <div className="text-white box-grey bg-slate-800">
+                        <h3>Subscribe</h3>
+                        <p className="font-light text-sm">Join Our Mailing List & to get our news.</p>
+                        <div className='flex pt-3'>
+                            <input type="text" placeholder='your email address' className="px-2 py-1 text-sm text-black outline-none"/>
+                            <button className="btn-footer">Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between pt-10 text-white text-sm text-slate-300">
+                    <span>Copyright &copy; <em className="text-teal-400">WhiteCollar</em> | Designed by <em className="text-purple-400">VictorFlow</em> Templates - Powered by Webflow</span>
+                    <ul className="flex">
+                        Follow: 
+                        <li className="text-white ml-2 socials">
+                            <Link href="/">
+                                <Image src={Instagram} alt="Instagram"/>
+                            </Link>
+                        </li>
+                        <li className="socials">
+                            <Link href="/">
+                                <Image src={Facebook} alt="Facebook"/>
+                            </Link>
+                        </li>
+                        <li className="socials">
+                            <Link href="/">
+                                <Image src={Twitter} alt="Twitter"/>
+                            </Link>
+                        </li>
+                        <li className="socials">
+                            <Link href="/">
+                                <Image src={Pinterest} alt="Pinterest"/>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
